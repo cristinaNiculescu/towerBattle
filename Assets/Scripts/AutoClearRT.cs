09b1a0c758bcdfe4107021807383432b5bbcec0c
Henrik Pedersen <henrikpedersen1@hotmail.com>
2015-11-09 17:43:27 +0100

    Added Fog-o-war element
    Created a basic foundation for Fog-Of-War feature

---
+++ Assets/Scripts/AutoClearRT.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Camera))]
+public class AutoClearRT : MonoBehaviour
+{
+    public bool noClearAfterStart = false;
+    // Use this for initialization
+    void Start()
+    {
+        GetComponent<Camera>().clearFlags = CameraClearFlags.Color;
+    }
+
+    void OnPostRender()
+    {
+        if (!noClearAfterStart)
+        {
+            GetComponent<Camera>().clearFlags = CameraClearFlags.Depth;
+        }
+    }
+}
