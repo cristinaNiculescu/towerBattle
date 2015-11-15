025e1c79b886a36ab10493a616c139b6d221c8da
Henrik Pedersen <henrikpedersen1@hotmail.com>
2015-11-15 22:49:48 +0100

    Added Rotation
    Rotation is now being synchronized for Players over the network :horse:

---
+++ Assets/Scripts/Player_SyncRotation.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class Player_SyncRotation : NetworkBehaviour 
+{
+    [SyncVar]
+    Quaternion syncPlayerCamRotation;
+
+    [SerializeField]
+    Transform playerCamTransform;
+    [SerializeField]
+    float lerpRate = 15f;
+	
+	void FixedUpdate () 
+    {
+        TransmitRotation();
+        LerpRotation();
+	}
+
+    void LerpRotation()
+    {
+        if(!isLocalPlayer)
+            playerCamTransform.rotation = Quaternion.Lerp(playerCamTransform.rotation, syncPlayerCamRotation, Time.deltaTime * lerpRate);
+    }
+
+    [Command]
+    void CmdProvideRotationToServer(Quaternion playerCamRotation)
+    {
+        syncPlayerCamRotation = playerCamRotation;
+    }
+
+    [ClientCallback]
+    void TransmitRotation()
+    {
+        if (isLocalPlayer)
+            CmdProvideRotationToServer(playerCamTransform.rotation);
+    }
+}
