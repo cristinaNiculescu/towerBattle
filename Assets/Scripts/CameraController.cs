d3b63ba94a875bcbbc7d93ba214d22ef0691fc92
Henrik Pedersen <henrikpedersen1@hotmail.com>
2015-11-15 18:29:02 +0100

    Made preperations for the Networking part
    Getting started with some basic netwroking setup with connecting 2
    players into one scene

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Camera))]
+public class CameraController : MonoBehaviour
+{
+
+    [Tooltip("Camera move speed")]
+    [Range(1, 10)]
+    public float speed = 3;
+    [Tooltip("Change how much zoom each 'tick' with the mousewheel does")]
+    [Range(30, 200)]
+    public float zoomRate = 50;
+    [Tooltip("how much can you zoom in")]
+    [Range(1, 99)]
+    public int closestZoom = 10;
+    [Tooltip("how much can you zoom out")]
+    [Range(100, 300)]
+    public int furthestZoom = 200;
+
+    private Camera cam;
+    private float size;
+
+    private float dragSpeed = 150;
+    private Vector3 dragOrigin;
+
+    void Start()
+    {
+        cam = GetComponent<Camera>();
+    }
+
+    void Update()
+    {
+
+    }
+
+    void LateUpdate()
+    {
+        Zoom();
+        DragCameraMiddleMouse();
+    }
+
+    /// <summary>
+    /// Draggin the camera around while holding down the mousewheel button.
+    /// </summary>
+    void DragCameraMiddleMouse()
+    {
+        if (Input.GetMouseButtonDown(2))
+        {
+            dragOrigin = Input.mousePosition;
+            return;
+        }
+        if (Input.GetMouseButton(2))
+        {
+            Vector3 pos = cam.ScreenToViewportPoint(Input.mousePosition - dragOrigin);
+            Vector3 move = new Vector3(pos.x, 0f, pos.y);
+            cam.transform.Translate(-move * Time.deltaTime * dragSpeed, Space.World);
+        }
+    }
+
+    /// <summary>
+    /// Zoom the orthograhic camera size by using the mousewheel
+    /// </summary>
+    void Zoom()
+    {
+        if ((Input.GetAxis("Mouse ScrollWheel") != 0f))
+        {
+            size = Mathf.Clamp(size - (Input.GetAxis("Mouse ScrollWheel") * zoomRate), closestZoom, furthestZoom);
+            cam.orthographicSize = size;
+        }
+    }
+}
