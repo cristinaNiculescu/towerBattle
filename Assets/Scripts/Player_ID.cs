118dac768bbddbf0ca55ad1088809022f9a5daf8
Henrik Pedersen <henrikpedersen1@hotmail.com>
2015-11-17 21:40:20 +0100

    Added Player_IDs for players when they join
    Just so each player will have e unique ID upon entering the game

---
+++ Assets/Scripts/Player_ID.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class Player_ID : NetworkBehaviour {
+
+    [SyncVar]
+    public string playerUniqueIdentity;
+    NetworkInstanceId playerNetID;
+    Transform myTransform;
+
+	void Awake () {
+        myTransform = transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (myTransform.name == "" || myTransform.name == "Player(Clone)")
+        {
+            SetIdentity();
+        }
+	}
+
+    public override void OnStartLocalPlayer()
+    {
+        GetNetIdentity();
+        SetIdentity();
+    }
+
+    [ClientCallback]
+    void GetNetIdentity()
+    {
+        playerNetID = GetComponent<NetworkIdentity>().netId;
+        CmdTellServerMyIdentity(MakeUniqueIdentity());
+    }
+
+    void SetIdentity()
+    {
+        if (!isLocalPlayer)
+        {
+            myTransform.name = playerUniqueIdentity;
+        }
+        else
+        {
+            myTransform.name = MakeUniqueIdentity();
+        }
+    }
+
+    string MakeUniqueIdentity()
+    {
+        string uniqueIdentity = "Player " + playerNetID.ToString();
+        return uniqueIdentity;
+    }
+
+    [Command]
+    void CmdTellServerMyIdentity(string identity)
+    {
+        playerUniqueIdentity = identity;
+    }
+}
