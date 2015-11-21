06ea3d704fd0a2a0ec9d855c467ae3981a98e843
Henrik Pedersen <henrikpedersen1@hotmail.com>
2015-11-21 11:53:26 +0100

    We are now have the foundation to spawn objects over the network
    :dancers:

---
+++ Assets/Scripts/Player_TestingShootBullet.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+using System.Collections;
+
+public class Player_TestingShootBullet : NetworkBehaviour {
+    
+    public GameObject bulletPrefab = null;
+
+    void Update()
+    {
+        // ... same as before
+        if (!isLocalPlayer)
+            return;
+        // when pressing the space button, ask the server to spawn a bullet
+        if (Input.GetKeyDown(KeyCode.Space))
+            Cmd_Shoot();
+    }
+
+    [Command]
+    public void Cmd_Shoot()
+    {
+        // create server-side instance
+        GameObject obj = (GameObject)Instantiate(bulletPrefab, new Vector3(Random.Range(-15f, 15f), 1f, Random.Range(-15f, 15)), Quaternion.identity);
+        // setup bullet component
+        TestingSpawnBullet bullet = obj.GetComponent<TestingSpawnBullet>();
+        bullet.velocity = transform.forward;
+        // destroy after 2 secs
+        Destroy(obj, 2.0f);
+        // spawn on the clients
+        NetworkServer.Spawn(obj);
+    }
+}
