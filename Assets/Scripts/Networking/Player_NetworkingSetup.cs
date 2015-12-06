74fb31223cc43f5c01442ac1c865fbf8e0a48473
Henrik Pedersen <henrikpedersen1@hotmail.com>
2015-12-06 14:42:09 +0100

    Followup on last commit
    Removed some unnecessary code...

--- Assets/Scripts/Networking/Player_NetworkingSetup.cs
+++ Assets/Scripts/Networking/Player_NetworkingSetup.cs
@@ -181,10 +181,6 @@ public class Player_NetworkingSetup : NetworkBehaviour
     void CmdSpawnBase(int spawnIndex, GameObject thePlayer)
     {
         GameObject baseSpawned = NetworkManager.singleton.spawnPrefabs[spawnIndex];
-        if (base.connectionToClient.connectionId == 2)//If player 2
-        {
-            baseSpawned.transform.position = new Vector3(Mathf.Abs(baseSpawned.transform.position.x), baseSpawned.transform.position.y, Mathf.Abs(baseSpawned.transform.position.z));
-        }
         NetworkServer.SpawnWithClientAuthority(baseSpawned, thePlayer);
     }
 
